ce40c3772a79df2723608551a337e1a72603cbc7
samy <samytwo22@gmail.com>
2023-10-21 19:47:43 +0200

    Amélioration page d'acceuil

--- js/btn.js
+++ js/btn.js
@@ -9,3 +9,47 @@ btn.addEventListener('click', () => {
     })
 
 })
+/*
+function ouvrirPage() {
+    var a = document.getElementById("search").value;
+
+    if (a === "home" || a === "ch*" || a === "chien") {
+        window.open("index.html");
+    }
+
+    else if (a === "Plougonven") {
+        window.open("commune.html");
+    }
+
+    else if (a === "lieu*") {
+        window.open("Ltouristique.html");
+    }
+    else if (a === "youtube") {
+        window.open("Service en ligne.html");
+    }
+
+
+}*/
+function ouvrirPage() {
+    var a = document.getElementById("search").value.toLowerCase().replace(/\s+/g, "");
+
+    if (a === "home" || a === "ch*" || a === "fdds") {
+        window.open("index.html");
+    }
+
+    else if (a === "plougonven") {
+        window.open("commune.html");
+    }
+
+    else if (a === "lieu*") {
+        window.open("Ltouristique.html");
+    }
+    else if (a === "youtube") {
+        window.open("Service en ligne.html");
+    }
+    else {
+        // Rediriger vers une page par défaut si aucune correspondance n'est trouvée
+        window.open("index.html");
+    }
+}
+
