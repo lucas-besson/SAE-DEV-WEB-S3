13f6fff21198617db2b22003bfe6cb36ad97c874
Damien <damien.morel@etu.u-paris.fr>
2023-10-25 23:59:13 +0200

    Amélioration des pages

--- js/btn.js
+++ js/btn.js
@@ -12,18 +12,18 @@ btn.addEventListener('click', () => {
 function ouvrirPage() {
     var a = document.getElementById("search").value.toLowerCase().replace(/\s+/g, "");
 
-    if (a === "home" || a.match(/^page/) || a.match(/^home/) || a.match(/^accueil/) || a === "page d'accueil" || a === "accueil" || a === "Morlaix") {
+    if (a === "home" || a.match(/^page/) || a.match(/^home/) || a.match(/^accueil/) || a === "accueil" || a === "morlaix" || a === "communauté") {
         window.open("index.html");
     }
 
-    else if (a === "plougonven" || a.match(/^page/) || a.match(/^commune/) || a.match(/^elu/) || a === "page commune" || a === "Commune" || a === "elu" || a === "elue" || a === "elus" || a === "elues" || a === "élus" || a === "élue" || a === "élues") {
+    else if (a === "plougonven" || a.match(/^commune/) || a.match(/^elu/) || a === "Commune" || a === "elu" || a === "elue" || a === "elus" || a === "elues" || a === "élus" || a === "élue" || a === "élues") {
         window.open("commune.html");
     }
 
-    else if (a === "lieux" || a.match(/^page/) || a.match(/^lieux/) || a.match(/^touristique/) || a === "touristiques" || a === "touristique" || a === "lieux touristiques") {
+    else if (a === "lieux" || a.match(/^lieux/) || a.match(/^touristique/) || a === "touristiques" || a === "touristique" || a === "lieux touristiques") {
         window.open("Ltouristique.html");
     }
-    else if (a === "service" || a.match(/^page/) || a.match(/^service/) || a.match(/^ligne/) || a.match(/^démarche/) || a === "service en ligne" || a === "démarche" || a === "demarche") {
+    else if (a === "service" || a.match(/^service/) || a.match(/^ligne/) || a.match(/^démarche/) || a === "service en ligne" || a === "démarche" || a === "demarche") {
         window.open("Service en ligne.html");
     }
     else {
